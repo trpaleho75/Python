8a2bbd2faf88f966ac5df2cc99b035e87e43082f
Travis <travisholt@hotmail.com>
2018-01-17 20:34:44 -0800

    Updated variables_start.

--- Learning Python/Exercise Files/Ch2/variables_start.py
+++ Learning Python/Exercise Files/Ch2/variables_start.py
@@ -2,3 +2,17 @@
 # Example file for variables
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
+
+# Declare a variable and initialize it
+f = 0
+print(f)
+
+
+# re-declaring the variable works
+f = "abc"
+print(f)
+
+
+# ERROR: variables of different types cannot be combined
+# print("string type " + 123)
+print("string type " + str(123))
